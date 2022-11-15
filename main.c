929c9f86d772e9dbbcd82d550c1273d1c5dd2e0e
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-11-15 16:41:37 +0900

    03

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+void swap(int *x,int *y ){
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
 void main(void) {
-	int i=300;
+	int a=3;
+	int b =5;
+	swap(&a,&b);
 	
-	int *pi=&i;
-	char *pc=&i;
-	
-	printf("%i,%i,%i\n",i,*pi,*pc);
+	printf("a:%i,b:%i\n",a,b);
 	
 }
