200bc01a3f1b00b2477ed69809472e4d596ec877
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-11-15 17:00:12 +0900

    05

--- main.c
+++ main.c
@@ -4,23 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int i;
-	int grade[5];
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i]=",i);
-		scanf("%d",&grade[i]);
-	}
+	pc=(char*)10000;
+	pi=(int*)10000;
+	pd=(double*)10000;
 	
-	int sum=0;
-	for(i=0;i<5;i++)
-	{
-		
-		printf("%i : %i\n",i, *(grade+i));
-		sum=sum+*(grade+i);
-	}
+	printf("증가전:pc= %d, pi=%d,pd=%d\n",pc,pi,pd);
 	
-	printf("average: %i\n",sum/5);
+	pc++;
+	pi++;
+	pd++;
+	printf("증가후: pc= %d, pi= %d, pd= %d\n",pc,pi,pd);
+	
 	return 0;
 }
